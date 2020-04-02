1a2520471cf49efdc788579a25fe80e8c952d9cf
Avery Gueth <35672644+averygue@users.noreply.github.com>
2020-04-01 23:37:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
+ 
+
 client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('ping');
-    }
-})
+ 
 
-client.login("bDbbjCBUGOuCbPirr7CLgjdii2awy7dr");
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
