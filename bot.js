8969690813e523a5afca3494990e64c1f2c6a480
Jl5her <Jl5her@yahoo.com>
2020-04-01 22:59:40 -0500

    processenv

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login("CLIb9psmfMqrM0m5rLsWLwx_lNsN7DS0");//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
