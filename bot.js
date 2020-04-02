2a07cc199b8c9ff535f432d9c66cdf1ea65560dc
Avery Gueth <35672644+averygue@users.noreply.github.com>
2020-04-01 23:53:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login("bDbbjCBUGOuCbPirr7CLgjdii2awy7dr");//BOT_TOKEN is the Client Secret
