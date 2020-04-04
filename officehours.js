45220837e4a3cfbe8e4c0b73979cbf1be5317d08
Avery Gueth <35672644+averygue@users.noreply.github.com>
2020-04-03 20:31:10 -0400

    Update officehours.js

--- officehours.js
+++ officehours.js
@@ -137,8 +137,8 @@ exports.onReady = (client, message) => {
   endTime = new Date();
   var timeDiff = endTime - startTime; //in ms
   timeDiff /= 1000;
-  var seconds = Math.round(timeDiff);
-  message.reply(seconds + " seconds");
+  var seconds = Math.round(timeDiff) / 60;
+  message.reply(seconds + " minutes");
     ready(message, 0)
 }
 
